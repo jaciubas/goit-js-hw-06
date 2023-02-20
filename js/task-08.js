aba5d562e3fca927b35781550fcb540fa33b7f7e
jaciubas <114225522+jaciubas@users.noreply.github.com>
2023-02-20 22:31:32 +0100

    Add files via upload

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
-
+const formLog = document.querySelector(".login-form");
+// const email = document.querySelector('[name="email"]');
+// const password = document.querySelector('[name="password"');
+// const btn = document.querySelector("button");
+formLog.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("fill in all fields");
+  } else {
+    formLog.reset();
+  }
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+});
